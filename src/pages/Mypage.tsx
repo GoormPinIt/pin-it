10bb038f6464ee18c3107e3504c23b391a490d40
hjoo830 <hjoo830@naver.com>
2024-12-16 17:35:26 +0900

    Feat: 마이페이지에 보드 UI 추가

--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -1,3 +1,86 @@
+import GridBoard from '../components/GridBoard';
+
+type BoardData = {
+  id: string;
+  title: string;
+  pinCount: number;
+  updatedTime: string;
+  images: string[];
+};
+
+// 더미 보드 데이터
+const boardData: BoardData[] = [
+  {
+    id: 'all-pins',
+    title: '모든 핀',
+    pinCount: 9,
+    updatedTime: '1시간',
+    images: [
+      'https://www.svgrepo.com/show/508699/landscape-placeholder.svg',
+      'https://www.svgrepo.com/show/508699/landscape-placeholder.svg',
+      'https://www.svgrepo.com/show/508699/landscape-placeholder.svg',
+      'https://www.svgrepo.com/show/508699/landscape-placeholder.svg',
+      'https://www.svgrepo.com/show/508699/landscape-placeholder.svg',
+      'https://www.svgrepo.com/show/508699/landscape-placeholder.svg',
+      'https://www.svgrepo.com/show/508699/landscape-placeholder.svg',
+      'https://www.svgrepo.com/show/508699/landscape-placeholder.svg',
+      'https://www.svgrepo.com/show/508699/landscape-placeholder.svg',
+    ],
+  },
+  {
+    id: 'cats',
+    title: '고양이',
+    pinCount: 6,
+    updatedTime: '1시간',
+    images: [
+      'https://www.svgrepo.com/show/508699/landscape-placeholder.svg',
+      'https://www.svgrepo.com/show/508699/landscape-placeholder.svg',
+      'https://www.svgrepo.com/show/508699/landscape-placeholder.svg',
+    ],
+  },
+  {
+    id: 'travel',
+    title: '여행',
+    pinCount: 8,
+    updatedTime: '2시간',
+    images: [
+      'https://www.svgrepo.com/show/508699/landscape-placeholder.svg',
+      'https://www.svgrepo.com/show/508699/landscape-placeholder.svg',
+      'https://www.svgrepo.com/show/508699/landscape-placeholder.svg',
+    ],
+  },
+];
+
+const Board = ({
+  title,
+  pinCount,
+  updatedTime,
+  images,
+}: Omit<BoardData, 'id'>): JSX.Element => (
+  <div className="w-56 flex flex-col">
+    {title === '모든 핀' ? (
+      <div className="relative w-full h-40">
+        {images.slice(0, 5).map((image, index) => (
+          <img
+            key={index}
+            src={image}
+            alt={`핀${5 - index}`}
+            className="absolute w-3/5 h-full object-cover rounded-2xl border-2 border-white"
+            style={{ left: `${index * 23}px`, zIndex: 5 - index }}
+          />
+        ))}
+      </div>
+    ) : (
+      <GridBoard images={images} />
+    )}
+    <h3 className="text-lg font-semibold mt-2">{title}</h3>
+    <div className="flex gap-2">
+      <p>핀 {pinCount}개</p>
+      <p className="text-gray-500">{updatedTime}</p>
+    </div>
+  </div>
+);
+
 const Mypage = (): JSX.Element => {
   return (
     <div className="p-4">
@@ -18,6 +101,12 @@ const Mypage = (): JSX.Element => {
             프로필 수정
           </button>
         </div>
+      </div>
+
+      <div className="flex flex-row flex-wrap gap-5">
+        {boardData.map(({ id, ...board }) => (
+          <Board key={id} {...board} />
+        ))}
       </div>
     </div>
   );
