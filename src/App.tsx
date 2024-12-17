ef5306b7e0538fdafadf2f62954f4821fa48eb60
조하은 <jhe93@naver.com>
2024-12-18 03:28:52 +0900

    add: 이미지 업로드 컴포넌트 작성
    핀 이미지를 업로드하는 컴포넌트를 작성하였습니다.

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Mypage from './pages/Mypage';
+import PinBuilder from './pages/PinBuilder';
 
 const App = (): JSX.Element => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/mypage" element={<Mypage />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/mypage" element={<Mypage />} />
+                <Route path="/mypage" element={<Mypage />} />
+                <Route path="/pin-creation-tool" element={<PinBuilder />} />
+            </Routes>
+        </Router>
+    );
 };
 
 export default App;
