41094c695e697ea74925da96da05ffb53a1db865
cho-eun-young <choeunyoung224@gmail.com>
2024-12-18 09:19:51 +0900

    feat : 로그인, 회원가입 기능

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Mypage from './pages/Mypage';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Mypage from "./pages/Mypage";
+import SignUp from "./pages/Signup";
 
 const App = (): JSX.Element => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/mypage" element={<Mypage />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/mypage" element={<Mypage />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 };
 
