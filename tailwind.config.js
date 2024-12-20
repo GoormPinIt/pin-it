af97e466ef3041d28ec4aa26ee5c91c9861a392e
gouoy <ga0cord@gmail.com>
2024-12-20 10:43:36 +0900

    add: style 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'btn_red':'#e60022', 
+        'btn_gray':'#e2e2e2',
+        'btn_h_red':'#b60000',
+        'btn_h_gray':'#e9e9e9', 
+        'pinit_yellow':'#fffd92',
+        'pinit_mint':'#d9fff6',
+        'pinit_pink':'#ffe2eb',
+        't2_red':'#c31852',
+        't3_green':'#016a6c',
+      },
+     
+    },
   },
   plugins: [],
 };
