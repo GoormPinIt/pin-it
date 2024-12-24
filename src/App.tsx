150bb08111e05c4e52d79ef604d5abd379b6cc6b
AnJuHyeon <anju3802@naver.com>
2024-12-24 23:03:15 +0900

    feat: 사이드바,디테일페이지 기능

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,31 @@ import { store } from "./store";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Mypage from "./pages/Mypage";
-import BoardDetails from './pages/BoardDetails';
+import BoardDetails from "./pages/BoardDetails";
 import SignUp from "./pages/Signup";
+import Sidebar from "./components/NavBar";
+import ProfileBoardDetail from "./pages/ProfileBoardDetail";
+
 
 const App = (): JSX.Element => {
+
   return (
     <Provider store={store}>
       <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/mypage" element={<Mypage />} />
-          <Route path="/board/:boardId" element={<BoardDetails />} />
-        </Routes>
+        <div className="flex">
+          <Sidebar />
+
+          <div className="flex-1 ml-10">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/mypage" element={<Mypage />} />
+              <Route path="/boardDetail" element={<ProfileBoardDetail />} />
+              <Route path="/board/:boardId" element={<BoardDetails />} />
+            </Routes>
+          </div>
+        </div>
       </Router>
     </Provider>
   );
