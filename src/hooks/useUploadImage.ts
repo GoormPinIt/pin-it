e5da06c61ec7ac91b1b59709ed99c2972b8bacd5
조하은 <jhe93@naver.com>
2024-12-31 19:32:14 +0900

    feat: 이미지 업로드 커스텀 훅 구현
    - Firebase Storage에 이미지를 업로드 커스텀 훅 구현
    - uuid 사용
    - 업로드 된 파일의 URL 반환 기능 구현

---
+++ src/hooks/useUploadImage.ts
@@ -0,0 +1,18 @@
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { v4 as uuidv4 } from "uuid";
+
+import { storage } from "../firebase";
+
+export default function useUploadImage() {
+  const storageRef = ref(storage, `uploads/${uuidv4()}`);
+  // firebase storage에 files이란 폴더가 만들어지고 그 안에 생성된 uuid 이름으로 이미지파일이 저장되게끔 한다.
+
+  const uploadImage = async (file: File) => {
+    const snapshot = await uploadBytes(storageRef, file);
+    const downloadUrl = await getDownloadURL(snapshot.ref);
+    console.log(downloadUrl);
+    return downloadUrl;
+  };
+
+  return uploadImage;
+}
