aade088ace03755944a2033a2a0c5d959404616a
gouoy <ga0cord@gmail.com>
2024-12-31 19:06:24 +0900

    add: 로그인 상태에 따른 화면

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store } from './store';
+import { Provider, useDispatch, useSelector } from 'react-redux';
+import { RootState, store } from './store';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Mypage from './pages/Mypage';
@@ -11,29 +11,53 @@ import Sidebar from './components/NavBar';
 import ProfileBoardDetail from './pages/ProfileBoardDetail';
 import Header from './components/Header';
 import LandingPage from './pages/LandingPage';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { loginSuccess, logout } from './features/authSlice';
 
 const App = (): JSX.Element => {
   return (
     <Provider store={store}>
       <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-        </Routes>
-        {/* <Header />
-        <div className="flex">
-          <Sidebar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/mypage" element={<Mypage />} />
-            <Route path="/board/:boardId" element={<BoardDetails />} />
-            <Route path="/boardDetail" element={<ProfileBoardDetail />} />
-          </Routes>
-        </div> */}
+        <AppRoutes />
       </Router>
     </Provider>
   );
 };
 
 export default App;
+
+const AppRoutes = (): JSX.Element => {
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
+  const auth = getAuth();
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch(loginSuccess(user.email || ''));
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, []);
+
+  return isLoggedIn ? (
+    <>
+      <Header />
+      <div className="flex">
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/mypage" element={<Mypage />} />
+          <Route path="/board/:boardId" element={<BoardDetails />} />
+          <Route path="/boardDetail" element={<ProfileBoardDetail />} />
+        </Routes>
+      </div>
+    </>
+  ) : (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
+  );
+};
