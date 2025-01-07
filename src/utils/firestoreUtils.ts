587550a360ffb15bc2c94aebc1244c3387e0c64e
조하은 <jhe93@naver.com>
2025-01-07 14:35:46 +0900

    add: 핀 저장 함수 구현
    보드에 핀을 잘 연결하도록 함수를 구현하였습니다.

--- src/utils/firestoreUtils.ts
+++ src/utils/firestoreUtils.ts
@@ -1,8 +1,35 @@
-import { addDoc, collection } from 'firebase/firestore';
+import {
+  addDoc,
+  collection,
+  doc,
+  updateDoc,
+  arrayUnion,
+} from 'firebase/firestore';
 import { db } from '../firebase';
 
 export const addPinToFirestore = async (pinData: any) => {
   const docRef = await addDoc(collection(db, 'pins'), pinData);
   console.log('Document written with ID:', docRef.id);
   return docRef.id;
+};
+
+export const addPinToBoard = async (
+  boardId: string,
+  pinId: string,
+): Promise<void> => {
+  if (!boardId || !pinId) {
+    console.error('Board ID or Pin ID is missing');
+    return;
+  }
+
+  try {
+    const boardRef = doc(db, 'boards', boardId); // Access the board document
+    await updateDoc(boardRef, {
+      pins: arrayUnion(pinId), // Add the pin ID to the pins array
+    });
+
+    console.log(`Pin ID: ${pinId} added to Board ID: ${boardId}`);
+  } catch (error) {
+    console.error('Error adding pin to board:', error);
+  }
 };
