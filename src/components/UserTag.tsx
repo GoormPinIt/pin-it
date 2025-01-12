593a3aa0df8263ccee5c21c1810ab5ca166a9fdb
조하은 <jhe93@naver.com>
2025-01-13 02:23:21 +0900

    fix: 핀 작성자 링크 추가
    핀 작성자의 닉네임/사진을 클릭하면 해당 유저의 페이지로 이동하도록 코드를 수정하였습니다.

--- src/components/UserTag.tsx
+++ src/components/UserTag.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import useFetchUserData from '../hooks/useFetchUserData';
+import { useNavigate } from 'react-router-dom';
 
 interface ProfileProps {
   uid: string;
@@ -7,14 +8,30 @@ interface ProfileProps {
 
 const UserTag: React.FC<ProfileProps> = ({ uid }) => {
   const { user, loading, error } = useFetchUserData(uid);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    console.log(uid);
+  }, []);
+
+  const handleClick = () => {
+    if (uid) {
+      console.log('uid:', uid);
+      navigate(`/profile/${uid}`); // uid를 포함한 경로로 이동
+    }
+  };
+
   return (
-    <header className="flex items-center mb-4">
+    <header
+      className="flex items-center mb-4 cursor-pointer"
+      onClick={handleClick}
+    >
       <figure className="w-8 h-8 rounded-full overflow-hidden mr-2">
         {user?.profileImage ? (
           <img src={user.profileImage} alt="사용자 프로필" />
         ) : (
           <img
-            src="https://eu.ui-avatars.com/api/?name=John+Doe&size=250"
+            src="https://i.pinimg.com/736x/3b/73/a1/3b73a13983f88f8b84e130bb3fb29e17.jpg"
             alt="사용자 프로필"
           />
         )}
