f42374b5bc1a1caca4e51e7d8639ce9b6de93272
cho-eun-young <choeunyoung224@gmail.com>
2025-02-19 15:31:21 +0900

    server

--- server/server.mjs
+++ server/server.mjs
@@ -27,6 +27,6 @@ io.on('connection', (socket) => {
 });
 
 const PORT = 4000;
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
